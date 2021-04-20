2560f1c7507a1264d8ca2f65f7a14d63a9ce7384
horix7 <horix77777@gmail.com>
2021-04-20 22:18:03 +0200

    initializing net server

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import net from 'net'
+
+let server = net.createServer( connection => {
+    console.log("connection made ...")
+    connection.on("data", data => {
+        // run this when data is received
+        console.log(data.toString()); // prints the data
+    });
+})
+
+server.listen(5000, () => {
+    console.log("server listening to port")
+})
+
