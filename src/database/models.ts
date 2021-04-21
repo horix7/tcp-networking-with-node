684864067a228ecde7dcb68718de133b71a18e6c
horix7 <horix77777@gmail.com>
2021-04-21 13:30:09 +0200

    modifying models

--- src/database/models.ts
+++ src/database/models.ts
@@ -2,7 +2,20 @@ import database from './database'
 
 export class UserModel {
 
-    public createUser = (username: string) => {
+    public createUser = async (username: string) => {
         
     }
 }
+
+interface room {
+    name: string,
+    madeby: string,
+    key: number
+}   
+
+export class Rooms {
+    public createRoom = async(body: room) => {
+
+    }
+}
+
